c19aea80aea755465944bf8abba7ea9d9f27da0e
Jin <jinxx493@umn.edu>
2014-12-08 20:18:16 -0600

    Update SimulationSICE.R

--- SimulationSICE.R
+++ SimulationSICE.R
@@ -153,3 +153,165 @@ fit.aic <- dpm(X1.t,X0.t,nlambda=100,tuning="aic");
 fit.cv <- dpm(X1.t,X0.t,nlambda=100,tuning="cv",folds=3);
 setwd("/home/merganser/jinjin/TestDE/lambda/Result")
 save(fit.aic,fit.cv, file="reallambda.RData")
+
+====================================================================================
+####### 3. SimulationAnalysis.R #######
+## ROC, Calculation of TPN etc.
+====================================================================================
+library(MASS);
+library(ROCR);
+p=74;
+n=180;
+n1=n*25;
+n0=n*30;
+nlambda=10;
+setwd("/home/merganser/jinjin/result")
+load("newdata2.RData")
+O1=out1$O1;
+O0=out1$O0;
+D0=O0-O1;
+setwd("/home/merganser/jinjin/TestDE/ROC")
+load("fitgenerate.RData")
+fit.aic =aic
+fit.cv =cv
+
+for (i in (1:10))           #####################absolute value!####################
+{fit.aic[[1]][[i]]=abs(fit.aic[[1]][[i]])
+fit.cv[[1]][[i]]=abs(fit.cv[[1]][[i]])
+}
+D0=abs(D0);
+####################   ROC   ###################
+setwd("/home/merganser/jinjin/TestDE/ROC")
+library(ROCR);
+######1######
+estimation=c(1:p*p);
+true=c(1:p*p);
+k=1;
+for (i in (1:(p)))
+{for (j in (1:(p)))
+{estimation[k]=eaic[[q]][i,j];
+true[k]=D0[i,j];
+k=k+1;
+}
+}
+for (i in (1:(p*p)))
+{if (abs(true[i])>1e-04){true[i]=1;}
+if (abs(true[i])<=1e-04){true[i]=0;}
+if (abs(estimation[i])>1e-04){estimation[i]=1;}
+if (abs(estimation[i])<=1e-04){estimation[i]=0;}
+}
+pred <- prediction(estimation,true)
+perf <- performance(pred,"tpr","fpr")
+pdf("ROCAIC1.pdf")
+plot(perf,colorize=TRUE)
+dev.off()
+
+############AIC############
+esaic=matrix(0,p,p*nlambda)
+list=list()
+k=c(1:6);
+for (i in (1:6))
+{k[i]=fit.aic[[4]][[i]];
+esaic[,(p*(i-1)+1):(p*i)] = fit.aic[[1]][[k[i]]]
+}
+eaic=list();
+eaic$a1=esaic[,(p*(1-1)+1):(p*1)];
+eaic$a2=esaic[,(p*(2-1)+1):(p*2)];
+eaic$a3=esaic[,(p*(3-1)+1):(p*3)];
+eaic$a4=esaic[,(p*(4-1)+1):(p*4)];
+eaic$a5=esaic[,(p*(5-1)+1):(p*5)];
+eaic$a6=esaic[,(p*(6-1)+1):(p*6)];
+############CV##############
+escv=matrix(0,p,p*nlambda)
+k=c(1:6);
+for (i in (1:6))
+{k[i]=fit.cv[[4]][[i]];
+escv[,(p*(i-1)+1):(p*i)] = fit.cv[[1]][[k[i]]]}
+ecv=list();
+ecv$a1=escv[,(p*(1-1)+1):(p*1)];
+ecv$a2=escv[,(p*(2-1)+1):(p*2)];
+ecv$a3=escv[,(p*(3-1)+1):(p*3)];
+ecv$a4=escv[,(p*(4-1)+1):(p*4)];
+ecv$a5=escv[,(p*(5-1)+1):(p*5)];
+ecv$a6=escv[,(p*(6-1)+1):(p*6)];
+############AIC############
+tpup=0;tpdown=0;
+tnup=0;tndown=0;
+tdup=0;tddown=0;
+tndup=0;tnddown=0;
+th=1.00000e-03  ### Specified by the user ###
+q=1;#######################Loss Function
+for (i in 1:(p))
+{
+for (j in 1:(p))
+{
+if ((eaic[[q]][i,j]>th)&&(D0[i,j]>th)){tpup=tpup+1;}
+if (D0[i,j]>th) {tpdown=tpdown+1;}
+if ((eaic[[q]][i,j]<=th)&&(D0[i,j]<=th)) {tnup=tnup+1;}
+if (D0[i,j]<=th){tndown=tndown+1;}
+if ((D0[i,j]>th)&&(eaic[[q]][i,j]>th)){tdup=tdup+1;}
+if (eaic[[q]][i,j]>th){tddown=tddown+1;}
+if ((eaic[[q]][i,j]<=th)&&(D0[i,j]<=th)) {tndup=tndup+1;}
+if (eaic[[q]][i,j]<=th){tnddown=tnddown+1;}
+}
+}
+
+tp=tpup/tpdown;
+tn=tnup/tndown;
+td=tdup/tddown;
+tnd=tndup/tnddown;
+
+###### Frobenius norm estimation accuracy:######
+Fnorm=0;
+for (i in 1:(p))
+{
+for (j in 1:(p))
+{
+Fnorm=Fnorm+(eaic[[q]][i,j]-D0[i,j])^2;
+}}
+Fnorm=sqrt(Fnorm);
+
+setwd("/home/merganser/jinjin/TestDE/Simulation/Result")
+save(tp,tn,td,tnd,Fnorm, file="truepositiverate_etc.RData")
+
+######################   CV   #######################
+q=fit.aic[[4]][[6]];  
+tpup2=0;tpdown2=0;
+tnup2=0;tndown2=0;
+tdup2=0;tddown2=0;
+tndup2=0;tnddown2=0;
+th=1.000000e-05  ### Specified by the user ###
+for (i in 1:(p))
+{
+for (j in 1:(p))
+{
+if ((ecv[[q]][i,j]>th)&&(D0[i,j]>th)) {tpup2=tpup2+1;}
+if (D0[i,j]>th) {tpdown2=tpdown2+1;}
+if ((ecv[[q]][i,j]<=th)&&(D0[i,j]<=th)) {tnup2=tnup2+1;}
+if (D0[i,j]<=th){tndown2=tndown2+1;}
+if ((D0[i,j]>th)&&(ecv[[q]][i,j]>th)){tdup2=tdup2+1;}
+if (ecv[[q]][i,j]>th){tddown2=tddown2+1;}
+if ((ecv[[q]][i,j]<=th)&&(D0[i,j]<=th)) {tndup2=tndup2+1;}
+if (ecv[[q]][i,j]<=th){tnddown2=tnddown2+1;}
+}
+}
+
+tp2=tpup2/tpdown2;
+tn2=tnup2/tndown2;
+td2=tdup2/tddown2;
+tnd2=tndup2/tnddown2;
+
+###### Frobenius norm estimation accuracy:######
+Fnorm2=0;
+for (i in 1:(p))
+{
+for (j in 1:(p))
+{
+Fnorm2=Fnorm2+(ecv[[q]][i,j]-D0[i,j])^2;
+}}
+Fnorm2=sqrt(Fnorm2);
+
+setwd("/home/merganser/jinjin/TestDE/Simulation/Result")
+save(tp2,tn2,td2,tnd2,Fnorm2,Fnorm2, file="truepositiverate2_etc.RData")
+
+
